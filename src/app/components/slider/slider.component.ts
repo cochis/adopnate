89bc7b08c0f82f79aa7bd66b4cc9f0a15b3a7761
Oscar Alejandro <ing.oarrs@gmail.com>
2021-07-06 23:29:29 -0500

    06/07/2021/2

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -34,8 +34,89 @@ export class SliderComponent implements OnInit {
       img: '/assets/img/perro4.jpg',
       titulo: 'Tu ubicación',
       desc: 'Siempre sabremos donde estás!'
+    },
+    {
+      img: '/assets/img/perro.jpg',
+      titulo: 'Comparte Fotos',
+      desc: 'Mira y comparte increíbles fotos de todo el mundo'
+    },
+    {
+      img: '/assets/img/perro1.jpg',
+      titulo: 'Escucha Música',
+      desc: 'Toda tu música favorita está aquí'
+    },
+    {
+      img: '/assets/img/perro2.jpg',
+      titulo: 'Nunca olvides nada',
+      desc: 'El mejor calendario del mundo a tu disposición'
+    },
+    {
+      img: '/assets/img/perro3.jpg',
+      titulo: 'Tu ubicación',
+      desc: 'Siempre sabremos donde estás!'
+    },
+    {
+      img: '/assets/img/perro4.jpg',
+      titulo: 'Tu ubicación',
+      desc: 'Siempre sabremos donde estás!'
+    },
+    {
+      img: '/assets/img/perro.jpg',
+      titulo: 'Comparte Fotos',
+      desc: 'Mira y comparte increíbles fotos de todo el mundo'
+    },
+    {
+      img: '/assets/img/perro1.jpg',
+      titulo: 'Escucha Música',
+      desc: 'Toda tu música favorita está aquí'
+    },
+    {
+      img: '/assets/img/perro2.jpg',
+      titulo: 'Nunca olvides nada',
+      desc: 'El mejor calendario del mundo a tu disposición'
+    },
+    {
+      img: '/assets/img/perro3.jpg',
+      titulo: 'Tu ubicación',
+      desc: 'Siempre sabremos donde estás!'
+    },
+    {
+      img: '/assets/img/perro4.jpg',
+      titulo: 'Tu ubicación',
+      desc: 'Siempre sabremos donde estás!'
+    },
+    {
+      img: '/assets/img/perro.jpg',
+      titulo: 'Comparte Fotos',
+      desc: 'Mira y comparte increíbles fotos de todo el mundo'
+    },
+    {
+      img: '/assets/img/perro1.jpg',
+      titulo: 'Escucha Música',
+      desc: 'Toda tu música favorita está aquí'
+    },
+    {
+      img: '/assets/img/perro2.jpg',
+      titulo: 'Nunca olvides nada',
+      desc: 'El mejor calendario del mundo a tu disposición'
+    },
+    {
+      img: '/assets/img/perro3.jpg',
+      titulo: 'Tu ubicación',
+      desc: 'Siempre sabremos donde estás!'
+    },
+    {
+      img: '/assets/img/perro4.jpg',
+      titulo: 'Tu ubicación',
+      desc: 'Siempre sabremos donde estás!'
     }
   ];
+  slideOptsOne = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true,
+    speed: 5000
+  };
   constructor(private navCtrl: NavController,
               private funService: FunctionsService,
               private auth: AuthService) {
